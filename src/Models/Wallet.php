374a644b7a8ebeca5fca009c978ad231489fc8cd
Ivan Krister Garcia <ivankrister.garcia@gmail.com>
2024-08-24 11:48:57 +0800

    wallet model

---
+++ src/Models/Wallet.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DotcodeIo\Wallet\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Wallet extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'uuid',
+        'user_id',
+        'balance',
+        'total_cash_in',
+        'total_cash_out',
+        'version',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'user_id' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function uniqueIds()
+    {
+        return ['uuid'];
+    }
+}
